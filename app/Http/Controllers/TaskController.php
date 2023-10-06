237c97ef5efe7df360c0ded5595f3ced35212f25
Ali Raza <alirazabinhussain@gmail.com>
2023-10-06 20:11:25 +0500

    Return tasks as per logged in user

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::where('user_id', Auth::user()->id)->get();
+
+        return view('tasks.index', compact('tasks'));
+    }
+}
